cbbc7705c4e1827e67268d3e5251d485cc66e81d
Drew Harris <drew.harris@ou.edu>
2020-02-29 14:15:23 -0600

    Added rotation based on arrow keys

--- eclipse_workspace/minesweeper_wrapped/src/minesweeper_wrapped/Cube.java
+++ eclipse_workspace/minesweeper_wrapped/src/minesweeper_wrapped/Cube.java
@@ -1,6 +1,9 @@
 package minesweeper_wrapped;
 
 import java.awt.DisplayMode;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLCapabilities;
@@ -14,7 +17,15 @@ import com.jogamp.opengl.util.FPSAnimator;
 public class Cube implements GLEventListener {
 	public static DisplayMode dm, dm_old;
 	private GLU glu = new GLU();
-	private float rquad = 0.0f;
+	private float xquad = 0.0f;
+	private float yquad = 0.0f;
+	private static float xrot = 0.0f;
+	private static float yrot = 0.0f;
+
+	final static GLProfile profile = GLProfile.get(GLProfile.GL2);
+	static GLCapabilities capabilities = new GLCapabilities(profile);
+	final static GLCanvas glcanvas = new GLCanvas(capabilities);
+	final static FPSAnimator animator = new FPSAnimator(glcanvas, 300, true);
 
 	@Override
 	public void display(GLAutoDrawable drawable) {
@@ -22,9 +33,10 @@ public class Cube implements GLEventListener {
 		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
 		gl.glLoadIdentity();
 		gl.glTranslatef(0f, 0f, -5.0f);
-// Rotate The Cube On X, Y & Z
-		gl.glRotatef(rquad, 1.0f, 1.0f, 1.0f);
-//giving different colors to different sides
+		// Rotate The Cube On X, Y & Z
+		gl.glRotatef(xquad, 0.0f, 1.0f, 0.0f);
+		gl.glRotatef(yquad, 1.0f, 0.0f, 0.0f);
+		//giving different colors to different sides
 		gl.glBegin(GL2.GL_QUADS); // Start Drawing The Cube
 		gl.glColor3f(1f, 0f, 0f); // red color
 		gl.glVertex3f(1.0f, 1.0f, -1.0f); // Top Right Of The Quad (Top)
@@ -58,12 +70,13 @@ public class Cube implements GLEventListener {
 		gl.glVertex3f(1.0f, -1.0f, -1.0f); // Bottom Right Of The Quad
 		gl.glEnd(); // Done Drawing The Quad
 		gl.glFlush();
-		rquad -= 0.15f;
+		xquad += xrot;
+		yquad += yrot;
 	}
 
 	@Override
 	public void dispose(GLAutoDrawable drawable) {
-// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 	}
 
 	@Override
@@ -93,10 +106,10 @@ public class Cube implements GLEventListener {
 	}
 
 	public static void main(String[] args) {
-		final GLProfile profile = GLProfile.get(GLProfile.GL2);
-		GLCapabilities capabilities = new GLCapabilities(profile);
-// The canvas
-		final GLCanvas glcanvas = new GLCanvas(capabilities);
+		//final GLProfile profile = GLProfile.get(GLProfile.GL2);
+		//GLCapabilities capabilities = new GLCapabilities(profile);
+		// The canvas
+		//final GLCanvas glcanvas = new GLCanvas(capabilities);
 		Cube cube = new Cube();
 		glcanvas.addGLEventListener(cube);
 		glcanvas.setSize(400, 400);
@@ -104,7 +117,58 @@ public class Cube implements GLEventListener {
 		frame.getContentPane().add(glcanvas);
 		frame.setSize(frame.getContentPane().getPreferredSize());
 		frame.setVisible(true);
-		final FPSAnimator animator = new FPSAnimator(glcanvas, 300, true);
-		animator.start();
+
+		KeyListener keyListener = new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				System.out.println(KeyEvent.getKeyText(e.getKeyCode()));
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+				{
+					xrot = 0.5f;
+					yrot = 0.0f;
+					animator.start();
+				}
+				
+				if (e.getKeyCode() == KeyEvent.VK_LEFT)
+				{
+					xrot = -0.5f;
+					yrot = 0.0f;
+					animator.start();
+				}
+				
+				if (e.getKeyCode() == KeyEvent.VK_UP)
+				{
+					yrot = 0.5f;
+				    xrot = 0.0f;
+					animator.start();
+				}
+				
+				if (e.getKeyCode() == KeyEvent.VK_DOWN)
+				{
+					yrot = -0.5f;
+					xrot = 0.0f;
+					animator.start();
+				}
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				animator.stop();
+			}
+
+		};
+
+		glcanvas.addKeyListener(keyListener);
+
+		//		final FPSAnimator animator = new FPSAnimator(glcanvas, 300, true);
+		//		animator.start();
 	}
 }
